350eaf44e2c14a8d8e0ed612913723114a80c1f4
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2021-11-10 15:04:40 -0300

    Kickoff

---
+++ src/main/java/br/com/digitalinnovation/abruzzo/project_cities_api/ProjectCitiesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.digitalinnovation.abruzzo.project_cities_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCitiesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digitalinnovation/abruzzo/project_cities_api/ProjectCitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalinnovation.abruzzo.project_cities_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

561f40b58ffc939a904a89f2a6cfb9e6916ce864
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2021-11-17 02:16:29 -0300

    Adiciona application.properties ao git

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,39 +1,3 @@
-
---
--- Inserindo dados na tabela "estado"
---
-
-INSERT INTO "estado" (id, nome, uf, ibge, pais, ddd) VALUES
-(1, 'Acre', 'AC', 12, 1, '[68]'),
-(2, 'Alagoas', 'AL', 27, 1, '[82]'),
-(3, 'Amazonas', 'AM', 13, 1, '[97,92]'),
-(4, 'Amapá', 'AP', 16, 1, '[96]'),
-(5, 'Bahia', 'BA', 29, 1, '[77,75,73,74,71]'),
-(6, 'Ceará', 'CE', 23, 1, '[88,85]'),
-(7, 'Distrito Federal', 'DF', 53, 1, '[61]'),
-(8, 'Espírito Santo', 'ES', 32, 1, '[28,27]'),
-(9, 'Goiás', 'GO', 52, 1, '[62,64,61]'),
-(10, 'Maranhão', 'MA', 21, 1, '[99,98]'),
-(11, 'Minas Gerais', 'MG', 31, 1, '[34,37,31,33,35,38,32]'),
-(12, 'Mato Grosso do Sul', 'MS', 50, 1, '[67]'),
-(13, 'Mato Grosso', 'MT', 51, 1, '[65,66]'),
-(14, 'Pará', 'PA', 15, 1, '[91,94,93]'),
-(15, 'Paraíba', 'PB', 25, 1, '[83]'),
-(16, 'Pernambuco', 'PE', 26, 1, '[81,87]'),
-(17, 'Piauí', 'PI', 22, 1, '[89,86]'),
-(18, 'Paraná', 'PR', 41, 1, '[43,41,42,44,45,46]'),
-(19, 'Rio de Janeiro', 'RJ', 33, 1, '[24,22,21]'),
-(20, 'Rio Grande do Norte', 'RN', 24, 1, '[84]'),
-(21, 'Rondônia', 'RO', 11, 1, '[69]'),
-(22, 'Roraima', 'RR', 14, 1, '[95]'),
-(23, 'Rio Grande do Sul', 'RS', 43, 1, '[53,54,55,51]'),
-(24, 'Santa Catarina', 'SC', 42, 1, '[47,48,49]'),
-(25, 'Sergipe', 'SE', 28, 1, '[79]'),
-(26, 'São Paulo', 'SP', 35, 1, '[11,12,13,14,15,16,17,18,19]'),
-(27, 'Tocantins', 'TO', 17, 1, '[63]'),
-(99, 'Exterior', 'EX', 99, NULL, NULL);
-
-
 --
 -- Inserindo dados na tabela "pais"
 --
@@ -294,6 +258,43 @@ INSERT INTO "pais" (id, nome, nome_pt, sigla, bacen) VALUES
 (261, 'Saint-Barthélemy', 'São Bartolomeu', 'FR', 6939),
 (262, 'Saint Martin', 'São Martinho, Ilha de (Parte Holandesa)', 'SM', 6998),
 (263, 'Terres Australes et Antarctiques Françaises', 'Terras Austrais e Antárcticas Francesas', 'TF', 7811);
+ON CONFLICT (did) DO NOTHING;
+
+--
+-- Inserindo dados na tabela "estado"
+--
+
+INSERT INTO "estado" (id, nome, uf, ibge, pais, ddd) VALUES
+(1, 'Acre', 'AC', 12, 1, '[68]'),
+(2, 'Alagoas', 'AL', 27, 1, '[82]'),
+(3, 'Amazonas', 'AM', 13, 1, '[97,92]'),
+(4, 'Amapá', 'AP', 16, 1, '[96]'),
+(5, 'Bahia', 'BA', 29, 1, '[77,75,73,74,71]'),
+(6, 'Ceará', 'CE', 23, 1, '[88,85]'),
+(7, 'Distrito Federal', 'DF', 53, 1, '[61]'),
+(8, 'Espírito Santo', 'ES', 32, 1, '[28,27]'),
+(9, 'Goiás', 'GO', 52, 1, '[62,64,61]'),
+(10, 'Maranhão', 'MA', 21, 1, '[99,98]'),
+(11, 'Minas Gerais', 'MG', 31, 1, '[34,37,31,33,35,38,32]'),
+(12, 'Mato Grosso do Sul', 'MS', 50, 1, '[67]'),
+(13, 'Mato Grosso', 'MT', 51, 1, '[65,66]'),
+(14, 'Pará', 'PA', 15, 1, '[91,94,93]'),
+(15, 'Paraíba', 'PB', 25, 1, '[83]'),
+(16, 'Pernambuco', 'PE', 26, 1, '[81,87]'),
+(17, 'Piauí', 'PI', 22, 1, '[89,86]'),
+(18, 'Paraná', 'PR', 41, 1, '[43,41,42,44,45,46]'),
+(19, 'Rio de Janeiro', 'RJ', 33, 1, '[24,22,21]'),
+(20, 'Rio Grande do Norte', 'RN', 24, 1, '[84]'),
+(21, 'Rondônia', 'RO', 11, 1, '[69]'),
+(22, 'Roraima', 'RR', 14, 1, '[95]'),
+(23, 'Rio Grande do Sul', 'RS', 43, 1, '[53,54,55,51]'),
+(24, 'Santa Catarina', 'SC', 42, 1, '[47,48,49]'),
+(25, 'Sergipe', 'SE', 28, 1, '[79]'),
+(26, 'São Paulo', 'SP', 35, 1, '[11,12,13,14,15,16,17,18,19]'),
+(27, 'Tocantins', 'TO', 17, 1, '[63]'),
+(99, 'Exterior', 'EX', 99, NULL, NULL)
+ON CONFLICT (did) DO NOTHING;
+
 
 
 
@@ -5896,5 +5897,5 @@ INSERT INTO "cidade" (id, nome, uf, ibge, lat_lon, latitude, longitude, cod_tom)
 (5606,	'Paraíso das Águas',	12,	5006275,	'(-19.0216007232666016,-53.0116004943848012)',	-1.90216007232666E+17,	-5.30116004943848E+17,	0),
 (5607,	'Pinto Bandeira',	23,	4314548,	'(-29.0974998474120987,-51.4502983093262003)',	-2.9097499847412E+17,	-5.14502983093262E+17,	1160),
 (5608,	'Balneário Rincão',	24,	4220000,	'(-28.8313999176025,-49.2351989746094034)',	-288313999176025,	-4.92351989746094E+17,	0),
-(5609,	'Pescaria Brava',	24,	4212650,	'(-28.3966007232666016,-48.8863983154297017)',	-2.83966007232666E+17,	-4.88863983154297E+17,	0);
-
+(5609,	'Pescaria Brava',	24,	4212650,	'(-28.3966007232666016,-48.8863983154297017)',	-2.83966007232666E+17,	-4.88863983154297E+17,	0)
+ON CONFLICT (did) DO NOTHING;
